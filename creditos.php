01abe949ea596b7613cdd7cb1cf542658b603a4a
GodCreepy147 <167023824+GodCreepy147@users.noreply.github.com>
2024-04-14 12:30:45 -0600

    Initial commit

---
+++ creditos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historial medico</title>
+
+    <!--Estilo de la pagína.-->
+    <link rel="stylesheet" href="./css/morado.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!--Icono de la pagína.-->
+    <link rel="shortcut icon" href="./img/images.png" type="image/x-icon">
+</head>
+<body>
+<div class="tit">
+    <br>
+    <h1>Historial medico</h1>
+    <br>
+</div>
+
+<!--Extras de la pagina.-->
+<div class="dat">
+    <br>
+    <p>
+        Agradecemos su colaboración de parte del cbtis <br>
+        05 su historial fue registrado de manera exitosa <br>
+        ya puede salir de la pagina :V.
+    </p>
+    <br>
+</div>
+
+<div class="tit">
+    <br>
+   <p>
+        <h3>JACYL</h3>
+        creditos al equipo jacyl conformado por: <br>
+            Luz Estrella Chavarria Hernández<br>
+            Yamir Caleb Diaz Escudero <br>
+            Maria Clara Villegas Castillo <br>
+            Alexa Daniela Barron Ibarra <br>
+            Jorge Hernández
+
+
+   </p>
+    <br>
+</div>
+</body>
+</html>
